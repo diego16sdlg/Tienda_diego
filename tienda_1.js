52dbf9a9bbefe3a17bb09128b11f4a7540c5e0a4
Diego Meza <101654213+diego16sdlg@users.noreply.github.com>
2024-04-21 19:51:44 -0500

    tienda_1.js

--- tienda_1.js
+++ tienda_1.js
@@ -1,16 +1,27 @@
+
 function mostrarResumen() {
     const nombreUsuario = document.getElementById('nombreUsuario').value;
     const bebidaSeleccionada = document.getElementById('bebidaSeleccionada').value;
-
-    if (nombreUsuario === "" || bebidaSeleccionada === "") {
+    const dniUsuario = document.getElementById('dniUsuario').value;
+    
+    if (nombreUsuario === "" || bebidaSeleccionada === "" || dniUsuario === "") {
         alert("¡Completa todos los campos para continuar!");
         return;
     }
+    
+      if (isNaN(dniUsuario)) {
+        alert("¡El DNI solo puede contener números!");
+        return;
+    }
+
 
     const nombreResumen = document.getElementById('nombreResumen');
+    const dniResumen = document.getElementById('dniResumen');
     const bebidaResumen = document.getElementById('bebidaResumen');
 
     nombreResumen.textContent = nombreUsuario;
+    dniResumen.textContent = 
+        dniUsuario;
 
     switch (bebidaSeleccionada) {
         case "cocacola":

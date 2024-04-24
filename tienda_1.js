a2e339997d6eb043a808c310afd11bb94fae4455
Diego Meza <101654213+diego16sdlg@users.noreply.github.com>
2024-04-23 19:12:36 -0500

    tienda_1.js

--- tienda_1.js
+++ tienda_1.js
@@ -1,47 +1,80 @@
 
-function mostrarResumen() {
-    const nombreUsuario = document.getElementById('nombreUsuario').value;
-    const bebidaSeleccionada = document.getElementById('bebidaSeleccionada').value;
-    const dniUsuario = document.getElementById('dniUsuario').value;
-    
-    if (nombreUsuario === "" || bebidaSeleccionada === "" || dniUsuario === "") {
-        alert("¡Completa todos los campos para continuar!");
-        return;
-    }
-    
-      if (isNaN(dniUsuario)) {
-        alert("¡El DNI solo puede contener números!");
-        return;
-    }
 
+function mostrarResumen() {
+
+    const nombreUsuario = document.getElementById('nombreUsuario').value;
+
+    const bebidaSeleccionada = document.getElementById('bebidaSeleccionada').value;
+
+    const dniUsuario = document.getElementById('dniUsuario').value;
+
+    
+
+function cerrarResumen() {
+
+    var resumenCompra = document.getElementById("resumenCompra");
+
+    resumenCompra.style.display = "none";
+
+}
 
     const nombreResumen = document.getElementById('nombreResumen');
+
     const dniResumen = document.getElementById('dniResumen');
+
     const bebidaResumen = document.getElementById('bebidaResumen');
 
+
+
     nombreResumen.textContent = nombreUsuario;
-    dniResumen.textContent = 
-        dniUsuario;
+
+    dniResumen.textContent = dniUsuario;
+
+
 
     switch (bebidaSeleccionada) {
+
         case "cocacola":
+
             bebidaResumen.textContent = "Coca-Cola";
+
             break;
+
         case "pepsi":
+
             bebidaResumen.textContent = "Pepsi";
+
             break;
+
         case "sprite":
+
             bebidaResumen.textContent = "Sprite";
+
             break;
+
         case "fanta":
+
             bebidaResumen.textContent = "Fanta";
+
             break;
+
         case "guarana":
+
             bebidaResumen.textContent = "Guaraná";
+
             break;
+
         default:
+
             bebidaResumen.textContent = "Bebida no seleccionada";
+
     }
 
+
+
     document.getElementById('resumenCompra').style.display = "block";
+
+    alert("¡Tu compra fue exitosa!");
+
 }
+
